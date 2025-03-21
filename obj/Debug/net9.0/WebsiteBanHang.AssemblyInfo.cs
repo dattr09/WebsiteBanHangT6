160c8102c40cd5b88a7aca5b11535c7f62670929
dattr09 <datsml0@gmail.com>
2025-03-21 11:11:22 +0700

    test

--- obj/Debug/net9.0/WebsiteBanHang.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebsiteBanHang.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bafab107713e4f733c066e36095252d9972be86")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593e27eb119b92546d4bb1e07995e14a0559ed1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebsiteBanHang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
